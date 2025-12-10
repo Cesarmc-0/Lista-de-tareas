80300a80bad51e469baca131dd17da21f7da80dd
CESAR <CESAR@DESKTOP-7GQ49RU>
2025-12-10 11:20:54 -0500

    nuevos iconos con funcionalidades

--- Lista de tareas/app.js
+++ Lista de tareas/app.js
@@ -2,36 +2,42 @@ const input = document.getElementById("ingresar-tarea");
 const boton = document.querySelector("button");
 const listaDeTareas = document.getElementById("lista-de-tareas");
 
-function agregarTarea(){
-     if(input.value){
+function agregarTarea() {
+   if (input.value) {
 
-    let tareaNueva = document.createElement("div");
-    tareaNueva.classList.add("tarea");
-    
-
-    let texto = document.createElement("p");
-    texto.textContent = input.value;
-    tareaNueva.appendChild(texto);
-    input.value = "";
-
-    let iconos = document.createElement("div");
-    iconos.classList.add("iconos");
-    tareaNueva.appendChild(iconos);
-
-    let completar = document.createElement("i");
-    completar.classList.add("bi", "bi-check-circle-fill","icono-completar");
-
-    let eliminar = document.createElement("i");
-    eliminar.classList.add("bi", "bi-trash3-fill","icono-eliminar");
-
-    iconos.appendChild(completar,eliminar);
+      let tareaNueva = document.createElement("div");
+      tareaNueva.classList.add("tarea");
 
 
-    listaDeTareas.appendChild(tareaNueva);
+      let texto = document.createElement("p");
+      texto.textContent = input.value;
+      tareaNueva.appendChild(texto);
+      input.value = "";
 
- } else {
-    alert("Por favor, ingresa una tarea.");
- }
+      let iconos = document.createElement("div");
+      iconos.classList.add("iconos");
+      tareaNueva.appendChild(iconos);
+
+      let completar = document.createElement("i");
+      completar.classList.add("bi", "bi-check-circle-fill", "icono-completar");
+      completar.addEventListener("click", () => {
+         texto.classList.toggle("completada");
+      });
+
+      let eliminar = document.createElement("i");
+      eliminar.classList.add("bi", "bi-trash3-fill", "icono-eliminar");
+      eliminar.addEventListener("click", () => {
+         tareaNueva.remove();
+      });
+
+      iconos.append(completar, eliminar);
+
+
+      listaDeTareas.appendChild(tareaNueva);
+
+   } else {
+      alert("Por favor, ingresa una tarea.");
+   }
 }
 
 boton.addEventListener("click", agregarTarea);
